2782d4fa5434e52c276f52ecc0b198a6b8684bda
leticialima6174286-boop <leticia.lima6174286@serra.pe.senac.br>
2026-08-30 16:13:22 -0300

    Add Funcionario class extending Usuario

--- biblioteca/Funcionario.java
+++ biblioteca/Funcionario.java
@@ -1,1 +1,26 @@
+public class Funcionario extends Usuario {
 
+    private String cargo;
+
+    public Funcionario(String nome, String cpf, String email, String cargo) {
+        super(nome, cpf, email);
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    @Override
+    public void listarInformacoes() {
+        System.out.println("Nome: " + getNome());
+        System.out.println("CPF: " + getCpf());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Cargo: " + cargo);
+        System.out.println();
+    }
+}
