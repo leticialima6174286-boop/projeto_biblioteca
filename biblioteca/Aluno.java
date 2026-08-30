50a63f2c625f15c7cf99b81938f354e87fb028c0
leticialima6174286-boop <leticia.lima6174286@serra.pe.senac.br>
2026-08-30 16:12:38 -0300

    Add Aluno class extending Usuario with course details

--- biblioteca/Aluno.java
+++ biblioteca/Aluno.java
@@ -1,1 +1,37 @@
+public class Aluno extends Usuario {
 
+    private String curso;
+    private String turma;
+
+    public Aluno(String nome, String cpf, String email, String curso, String turma) {
+        super(nome, cpf, email);
+        this.curso = curso;
+        this.turma = turma;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    @Override
+    public void listarInformacoes() {
+        System.out.println("Nome: " + getNome());
+        System.out.println("CPF: " + getCpf());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Curso: " + curso);
+        System.out.println("Turma: " + turma);
+        System.out.println();
+    }
+}
