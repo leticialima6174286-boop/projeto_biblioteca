d08449aa4811ae5c49399257617bb4f65b54a1d4
leticialima6174286-boop <leticia.lima6174286@serra.pe.senac.br>
2026-08-30 16:09:10 -0300

    Implement Livro class with availability methods

--- biblioteca/Livro.java
+++ biblioteca/Livro.java
@@ -1,1 +1,52 @@
+public class Livro implements Disponibilidade {
 
+    private String titulo;
+    private String autor;
+    private int ano;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        disponivel = true;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    @Override
+    public void emprestar() {
+        disponivel = false;
+    }
+
+    @Override
+    public void devolver() {
+        disponivel = true;
+    }
+}
